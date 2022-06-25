c3e21a328992bcb3830fba09527cafdc8f473ad0
CJRadit <cjradit@outlook.co.id>
2022-06-25 17:15:22 +0700

    Update tampilan tes

--- resources/views/tes/tes.blade.php
+++ resources/views/tes/tes.blade.php
@@ -1,4 +1,4 @@
-@extends('layouts.tes')
+@extends('layouts.front')
 
 @section('content')
 <div class="content-header">
@@ -15,26 +15,27 @@
     <div class="container">
         <div class="row">
             <div class="card card-outline card-primary col-12">
-                <form action="{{ Request::url() }}" method="post"></form>
-                <div class="card-body">
-                    @php
-                        $nomor_soal = 1;
-                    @endphp
-                    @foreach ($soal as $s)
-                        <div class="form-group">
-                            <p>{{ $nomor_soal++ }}.) {{ $s['pertanyaan'] }}</p>
-                            @foreach ($s['opsi'] as $so)
-                                <div class="form-check">
-                                    <input class="form-check-input" type="radio" name="{{ $s['id'] }}" id="soal_opsi_id_{{ $so['id'] }}" value="{{ $so['id'] }}">
-                                    <label class="form-check-label" for="opsi_id_{{ $so['id'] }}">{{ $so['soal_opsi'] }}</label>
-                                </div>
-                            @endforeach
-                        </div>
-                    @endforeach
-                </div>
-                <div class="card-footer">
-                    <button type="submit" class="btn btn-primary">Kumpulkan Tes</button>
-                </div>
+                <form action="{{ Request::url() }}" method="post">
+                    <div class="card-body">
+                        @php
+                            $nomor_soal = 1;
+                        @endphp
+                        @foreach ($soal as $s)
+                            <div class="form-group">
+                                <p>{{ $nomor_soal++ }}.) {{ $s['pertanyaan'] }}</p>
+                                @foreach ($s['opsi'] as $so)
+                                    <div class="form-check">
+                                        <input class="form-check-input" type="radio" name="{{ $s['id'] }}" id="soal_opsi_id_{{ $so['id'] }}" value="{{ $so['id'] }}">
+                                        <label class="form-check-label" for="opsi_id_{{ $so['id'] }}">{{ $so['soal_opsi'] }}</label>
+                                    </div>
+                                @endforeach
+                            </div>
+                        @endforeach
+                    </div>
+                    <div class="card-footer">
+                        <button type="submit" class="btn btn-primary">Kumpulkan Tes</button>
+                    </div>
+                </form>
             </div>
         </div>
     </div>
