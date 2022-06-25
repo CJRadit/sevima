d681e31834635b4459d82c8975820987291a687d
CJRadit <cjradit@outlook.co.id>
2022-06-25 22:01:46 +0700

    Update migration

--- database/migrations/2022_06_25_024147_create_user_kelas.php
+++ database/migrations/2022_06_25_024147_create_user_kelas.php
@@ -14,6 +14,7 @@ return new class extends Migration
     public function up()
     {
         Schema::create('user_kelas', function (Blueprint $table) {
+            $table->id();
             $table->foreignId('user_id')->constrained('users');
             $table->foreignId('kelas_id')->constrained('kelas');
             $table->boolean('is_owner')->nullable()->default(false);
